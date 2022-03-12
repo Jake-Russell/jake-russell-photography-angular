3090e4b24a06b880e2887a09cc3012a34b70f785
Jake Russell <jake@megster.net>
2022-03-12 11:07:50 +0000

    Fleshed out navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
@@ -14,6 +17,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'navbar',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isMatMenuOpen = false;
+  enteredButton = false;
+  prevButtonTrigger!: MatMenuTrigger;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  menuLeave(trigger: MatMenuTrigger) {
+    setTimeout(() => {
+      this.isMatMenuOpen = false;
+      trigger.closeMenu();
+    }, 100);
+  }
+
+  buttonEnter(trigger: MatMenuTrigger) {
+    setTimeout(() => {
+      this.enteredButton = true;
+      this.prevButtonTrigger = trigger;
+      trigger.openMenu();
+    }, 100);
+  }
+
+  buttonLeave(trigger: MatMenuTrigger) {
+    setTimeout(() => {
+      if (!this.isMatMenuOpen) {
+        trigger.closeMenu();
+      }
+    }, 100);
+  }
 }
