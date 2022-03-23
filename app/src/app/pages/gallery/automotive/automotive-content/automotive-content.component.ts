64628fb13babcfe5ca35faf12b8084c465f485a0
Jake Russell <jake@megster.net>
2022-03-23 14:17:47 +0000

    Added Ferrari Racing Days photo tile

--- app/src/app/pages/gallery/automotive/automotive-content/automotive-content.component.ts
+++ app/src/app/pages/gallery/automotive/automotive-content/automotive-content.component.ts
@@ -18,6 +18,15 @@ export class AutomotiveContentComponent implements OnInit {
         '../../../../assets/images/labels/automotive_salon_prive_2021_label_hover.png',
       galleryUrl: '/gallery/automotive/salon_prive_2021',
     },
+    {
+      imageSrc:
+        '../../../../assets/images/automotive/ferrari_racing_days_2021/ferrari_fxxk_evo_red_2.jpg',
+      labelSrc:
+        '../../../../assets/images/labels/automotive_ferrari_racing_days_2021_label.png',
+      labelHoverSrc:
+        '../../../../assets/images/labels/automotive_ferrari_racing_days_2021_label_hover.png',
+      galleryUrl: '/gallery/automotive/salon_prive_2021',
+    },
   ];
 
   constructor() {}
