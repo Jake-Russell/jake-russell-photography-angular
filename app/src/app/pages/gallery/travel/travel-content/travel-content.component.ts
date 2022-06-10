b6b02139b02d119553b6fee5b72dd800351856b3
Jake Russell <jake@Jakes-iMac.local>
2022-06-10 17:15:29 +0100

    Updated travel gallery

--- app/src/app/pages/gallery/travel/travel-content/travel-content.component.ts
+++ app/src/app/pages/gallery/travel/travel-content/travel-content.component.ts
@@ -7,6 +7,43 @@ import { ImageThumbnail } from 'src/app/image-thumbnail.model';
   styleUrls: ['./travel-content.component.css'],
 })
 export class TravelContentComponent implements OnInit {
+  carouselImages = [
+    {
+      imageSrc: '../../../assets/images/travel/budapest/budapest_8.jpg',
+      imageAlt: '',
+    },
+    {
+      imageSrc: '../../../assets/images/travel/bratislava/bratislava_8.jpg',
+      imageAlt: '',
+    },
+    {
+      imageSrc: '../../../assets/images/travel/prague/prague_12.jpg',
+      imageAlt: '',
+    },
+    {
+      imageSrc: '../../../assets/images/travel/brussels/brussels_9.jpg',
+      imageAlt: '',
+    },
+    {
+      imageSrc: '../../../assets/images/travel/paris/eiffel_tower_2.jpg',
+      imageAlt: '',
+    },
+    {
+      imageSrc: '../../../assets/images/travel/albufeira/rocks_8.jpg',
+      imageAlt: '',
+    },
+  ];
+
+  galleryTitle = 'Travel Galleries';
+
+  galleryText = [
+    'Travel is a huge passion of mine, and I love to explore all different parts of the world.',
+    "Wherever I go, I make sure that my trusty camera is always to hand so that I can capture some of the world's most beautiful landscapes and architecture.",
+    'In the Summer of 2019, I went on an Interrailing trip around Europe, visiting Budapest, Bratislava, Prague, Munich, Cologne and finally Brussels. This fantastic trip really kick started my passion for travel, and my list of places to visit is ever growing!',
+    'Unfortunately shortly after the Interrailing trip, Covid hit. This put a stop to any travel plans I may have wanted to persue.',
+    'In 2022, I have been quick to pick up my passion again, and have visited Paris in February, and Albufeira in May.',
+  ];
+
   photoListImages: ImageThumbnail[] = [
     {
       imageSrc:
