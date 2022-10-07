6c6957221c38c8b959003387d49c5b8543bc69d1
Jake Russell <jake@megster.net>
2022-10-07 12:36:32 +0100

    Added images

--- app/src/app/pages/gallery/travel/london-content/london-content.component.ts
+++ app/src/app/pages/gallery/travel/london-content/london-content.component.ts
@@ -25,6 +25,262 @@ export class LondonContentComponent implements OnInit {
       imageAlt: '',
       ratio: Ratio.Portrait,
     },
+    {
+      imageSrc: '../../../assets/images/travel/london/gherkin.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/harrods_1.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/harrods_2.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/leadenhall_market_1.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/leadenhall_market_2.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_1_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_1_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_2.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_3_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_3_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_4_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_4_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_5_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_5_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_6_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_6_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_7_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_7_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_8.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_9_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_9_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_10_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_10_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_11_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/leake_street_arches_11_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_architecture_1.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_architecture_2.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_architecture_3_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_architecture_3_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_architecture_4.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/london_eye_1.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/london_eye_2_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/london_eye_2_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_1.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/sky_garden_2_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_2_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/sky_garden_3_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_3_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_4.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/sky_garden_5_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_5_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sky_garden_6.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc: '../../../assets/images/travel/london/sloane_street.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/st_dunstan_in_the_east_churchyard_black_and_white.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
+    {
+      imageSrc:
+        '../../../assets/images/travel/london/st_dunstan_in_the_east_churchyard_colour.jpg',
+      imageAlt: '',
+      ratio: Ratio.Portrait,
+    },
   ];
 
   constructor() {}
